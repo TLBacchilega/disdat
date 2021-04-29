c8d394b72c25d305b16eb46104f510218c3014ed
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-04-29 15:27:54 +0200

    Update R_code_prova.r

--- R_code_prova.r
+++ R_code_prova.r
@@ -1,7 +1,7 @@
 ## Data Visualisation for disdat data
 
 # https://cran.r-project.org/web/packages/disdat/index.html
-# disdat: Data for Comparing Species Distribution Modeling Methods,These data are described and made available by Elith et al. (2020) https://doi.org/10.17161%2Fbi.v15i2.13384
+# disdat: Data for Comparing Species Distribution Modeling Methods,These data are described and made available by Elith et al. (2020) https://doi.org/10.17161%2Fbi.v15i2.13384  
 
 install.packages("disdat")
 library(disdat)
