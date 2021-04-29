c45107b48b8a3fce3b730a2bd9bf015877e46562
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-04-29 16:08:26 +0200

    Update R_code_prova.r

--- R_code_prova.r
+++ R_code_prova.r
@@ -2,6 +2,8 @@
 
 # https://cran.r-project.org/web/packages/disdat/index.html
 # disdat: Data for Comparing Species Distribution Modeling Methods,These data are described and made available by Elith et al. (2020) https://doi.org/10.17161%2Fbi.v15i2.13384  
+# The dataset is available on Open Science Framework and as an R package and can be used as a benchmark for modeling approaches and for testing new ways to evaluate the accuracy of SDMs.
+
 
 install.packages("disdat")
 library(disdat)
