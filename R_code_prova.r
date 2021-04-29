5d8ab8fa9e1455cb7a2c6545c97442c5c02d411b
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-04-29 15:20:26 +0200

    Update R_code_prova.r

--- R_code_prova.r
+++ R_code_prova.r
@@ -22,6 +22,7 @@ install.packages("sf")
 library(sf)
 # Linking to GEOS 3.9.0, GDAL 3.2.1, PROJ 7.2.1
 
+# https://www.rdocumentation.org/packages/tidyverse/versions/1.3.1
 install.packages("tidyverse")
 library(tidyverse)
 # Errore: package or namespace load failed for ‘tidyverse’ in loadNamespace(j <- i[[1L]], c(lib.loc, .libPaths()), versionCheck = vI[[j]]):
@@ -33,7 +34,7 @@ remove.packages("pillar")
 install.packages("pillar")
 library(pillar)
 install.packages("tidyverse")
-library(tidyverse)
+library(tidyverse) # caricherà i pacchetti di riordino principali:
 # -- Attaching packages --------------------------------------- tidyverse 1.3.1 --
 # v ggplot2 3.3.3     v purrr   0.3.4
 # v tibble  3.1.0     v dplyr   1.0.5
@@ -73,63 +74,6 @@ library(sf)
 
 
 
-### Modeling NCEAS data
-
-library(disdat) # pacchetto già installato
-install.packages("forcats")
-library(forcats) # for handling factor variables
-
-# https://www.rdocumentation.org/packages/randomForest/versions/4.6-14/topics/randomForest
-
-install.packages("randomForest") # Classification and Regression with Random Forest
-library(randomForest)
-# randomForest 4.6-14
-# Type rfNews() to see new features/changes/bug fixes.
-
-#Attaching package: ‘randomForest’
-
-#The following object is masked from ‘package:ggplot2’:
-
-    #margin
-
-#Warning message:
-#package ‘randomForest’ was built under R version 4.0.5 
-
-
-
-install.packages("dplyr")
-# Warning message:
-#In file.copy(savedcopy, lib, recursive = TRUE) :
-  #problem copying C:\Users\User\Documents\R\win-library\4.0\00LOCK\dplyr\libs\x64\dplyr.dll to C:\Users\User\Documents\R\win-library\4.0\dplyr\libs\x64\dplyr.dll: Permission denied
-
-library(dplyr)
-# Attaching package: ‘dplyr’
-
-#The following object is masked from ‘package:randomForest’:
-
-    #combine
-
-#The following objects are masked from ‘package:stats’:
-
-    #filter, lag
-
-#The following objects are masked from ‘package:base’:
-
-    #intersect, setdiff, setequal, union
-
-
-
-
-
-install.packages("ROCR")
-library(ROCR)
-
-
-
-# Creating target group background (TGB) sample
-
-install.packages("raster")
-library(raster)
 
 
 
